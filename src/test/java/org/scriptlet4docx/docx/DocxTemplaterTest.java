0fc61d1ddd08d48798edfe62d79ac43f359de256
eg <blackorangebox@gmail.com>
2013-01-24 13:50:41 +0700

    updated testProcess test so result docx can be viewed by hand

--- src/test/java/org/scriptlet4docx/docx/DocxTemplaterTest.java
+++ src/test/java/org/scriptlet4docx/docx/DocxTemplaterTest.java
@@ -14,207 +14,207 @@ import org.scriptlet4docx.util.test.TestUtils;
 
 public class DocxTemplaterTest {
 
-	static HashMap<String, Object>	params;
+    static HashMap<String, Object> params;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		params = new HashMap<String, Object>();
-		HashMap<String, String> contract = new HashMap<String, String>();
-		contract.put("number", "123#445");
-		params.put("value", 1);
-		params.put("contract", contract);
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        params = new HashMap<String, Object>();
+        HashMap<String, String> contract = new HashMap<String, String>();
+        contract.put("number", "123#445");
+        params.put("value", 1);
+        params.put("contract", contract);
 
-		List<String> personList = new ArrayList<String>();
-		personList.add("vasya");
-		personList.add("petya");
+        List<String> personList = new ArrayList<String>();
+        personList.add("vasya");
+        personList.add("petya");
 
-		params.put("personList", personList);
-		params.put("menList", personList);
-		
-		List<HashMap<String, Object>> employeeList = new ArrayList<HashMap<String, Object>>();
-		HashMap<String, Object> p1 = new HashMap<String, Object>();
-		p1.put("name", "Tom");
-		p1.put("address", "Moscow");
-		HashMap<String, Object> p2 = new HashMap<String, Object>();
-		p2.put("name", "John");
-		p2.put("address", "New York");
-		employeeList.add(p1);
-		employeeList.add(p2);
+        params.put("personList", personList);
+        params.put("menList", personList);
 
-		params.put("employeeList", employeeList);
-	}
+        List<HashMap<String, Object>> employeeList = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> p1 = new HashMap<String, Object>();
+        p1.put("name", "Tom");
+        p1.put("address", "Moscow");
+        HashMap<String, Object> p2 = new HashMap<String, Object>();
+        p2.put("name", "John");
+        p2.put("address", "New York");
+        employeeList.add(p1);
+        employeeList.add(p2);
 
-	@Test
-	public void testProcessScriptedTemplate() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-1.xml");
+        params.put("employeeList", employeeList);
+    }
 
-		String result = DocxTemplater.processScriptedTemplate(template, params);
+    @Test
+    public void testProcessScriptedTemplate() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-1.xml");
 
-		assertTrue(result != null);
-		assertTrue(result.contains("123#445"));
-		assertTrue(StringUtils.countMatches(result, "123#445") == 4);
-	}
+        String result = DocxTemplater.processScriptedTemplate(template, params);
 
-	@Test
-	public void testProcessScriptedTemplate_brokenType1() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-2.xml");
+        assertTrue(result != null);
+        assertTrue(result.contains("123#445"));
+        assertTrue(StringUtils.countMatches(result, "123#445") == 4);
+    }
 
-		String result = DocxTemplater.processScriptedTemplate(template, params);
+    @Test
+    public void testProcessScriptedTemplate_brokenType1() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-2.xml");
 
-		assertTrue(result != null);
-		assertTrue(result.contains("123#445"));
-		assertTrue(StringUtils.countMatches(result, "123#445") == 1);
-	}
+        String result = DocxTemplater.processScriptedTemplate(template, params);
 
-	@Test
-	public void testPreProcessTableScripting_multiTr() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-5.xml");
+        assertTrue(result != null);
+        assertTrue(result.contains("123#445"));
+        assertTrue(StringUtils.countMatches(result, "123#445") == 1);
+    }
 
-		String result = TableScriptingProcessor.process(template);
+    @Test
+    public void testPreProcessTableScripting_multiTr() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-5.xml");
 
-		assertTrue(result != null);
-		assertTrue(!result.contains("$["));
+        String result = TableScriptingProcessor.process(template);
 
-		assertTrue(result.contains("&lt;% def iterStatus=0; for ( wawawa in tour.wawawa ) { iterStatus++; %&gt;"));
-		assertTrue(result.contains("${wawawa.myway}"));
+        assertTrue(result != null);
+        assertTrue(!result.contains("$["));
 
-		assertTrue(result.contains("&lt;% iterStatus=0; for ( mamama in tour1.mamama ) { iterStatus++; %&gt;"));
-		assertTrue(result.contains("${mamama.myway}"));
+        assertTrue(result.contains("&lt;% def iterStatus=0; for ( wawawa in tour.wawawa ) { iterStatus++; %&gt;"));
+        assertTrue(result.contains("${wawawa.myway}"));
 
-	}
+        assertTrue(result.contains("&lt;% iterStatus=0; for ( mamama in tour1.mamama ) { iterStatus++; %&gt;"));
+        assertTrue(result.contains("${mamama.myway}"));
 
-	@Test
-	public void testProcessScriptedTemplate_tableScripting() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-6.xml");
+    }
 
-		String result = DocxTemplater.processScriptedTemplate(template, params);
+    @Test
+    public void testProcessScriptedTemplate_tableScripting() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-6.xml");
 
-		assertTrue(result != null);
-		assertTrue(!result.contains("$["));
+        String result = DocxTemplater.processScriptedTemplate(template, params);
 
-		assertTrue(result.contains(">vasya<"));
-		assertTrue(result.contains(">petya<"));
+        assertTrue(result != null);
+        assertTrue(!result.contains("$["));
 
-	}
+        assertTrue(result.contains(">vasya<"));
+        assertTrue(result.contains(">petya<"));
 
-	@Test
-	public void testProcessScriptedTemplate_brokenType2() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-3.xml");
+    }
 
-		String result = DocxTemplater.processScriptedTemplate(template, params);
+    @Test
+    public void testProcessScriptedTemplate_brokenType2() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-3.xml");
 
-		assertTrue(result != null);
-		assertTrue(result.contains("123#445"));
-		assertTrue(StringUtils.countMatches(result, "123#445") == 1);
-	}
+        String result = DocxTemplater.processScriptedTemplate(template, params);
 
-	@Test
-	public void testProcessScriptedTemplate_brokenType2_noProcess() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-7.xml");
+        assertTrue(result != null);
+        assertTrue(result.contains("123#445"));
+        assertTrue(StringUtils.countMatches(result, "123#445") == 1);
+    }
 
-		String result = DocxTemplater.processScriptedTemplate(template, params);
+    @Test
+    public void testProcessScriptedTemplate_brokenType2_noProcess() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-7.xml");
 
-		assertTrue(result != null);
-		assertTrue(template.equals(result));
-	}
+        String result = DocxTemplater.processScriptedTemplate(template, params);
 
-	@Test
-	public void testProcessScriptedTemplate_tableScripting_iterStatus() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-8.xml");
+        assertTrue(result != null);
+        assertTrue(template.equals(result));
+    }
 
-		String result = DocxTemplater.processScriptedTemplate(template, params);
+    @Test
+    public void testProcessScriptedTemplate_tableScripting_iterStatus() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-8.xml");
 
-		assertTrue(result != null);
-		assertTrue(!result.contains("$["));
+        String result = DocxTemplater.processScriptedTemplate(template, params);
 
-		assertTrue(result.contains(">vasya<"));
-		assertTrue(result.contains(">petya<"));
+        assertTrue(result != null);
+        assertTrue(!result.contains("$["));
 
-		assertTrue(!result.contains(">${iterStatus}<"));
+        assertTrue(result.contains(">vasya<"));
+        assertTrue(result.contains(">petya<"));
 
-		assertTrue(result.contains(">1<"));
-		assertTrue(result.contains(">2<"));
+        assertTrue(!result.contains(">${iterStatus}<"));
 
-	}
+        assertTrue(result.contains(">1<"));
+        assertTrue(result.contains(">2<"));
 
-	@Test
-	public void testProcessScriptedTemplate_tableScripting_iterStatus_multiTable() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-4.xml");
+    }
 
-		String result = DocxTemplater.processScriptedTemplate(template, params);
+    @Test
+    public void testProcessScriptedTemplate_tableScripting_iterStatus_multiTable() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-4.xml");
 
-		assertTrue(result != null);
-		assertTrue(!result.contains("$["));
+        String result = DocxTemplater.processScriptedTemplate(template, params);
 
-		assertTrue(result.contains(">vasya<"));
-		assertTrue(result.contains(">petya<"));
+        assertTrue(result != null);
+        assertTrue(!result.contains("$["));
 
-		assertTrue(!result.contains(">${iterStatus}<"));
+        assertTrue(result.contains(">vasya<"));
+        assertTrue(result.contains(">petya<"));
 
-		assertTrue(result.contains(">1<"));
-		assertTrue(result.contains(">2<"));
+        assertTrue(!result.contains(">${iterStatus}<"));
 
-		assertTrue(StringUtils.countMatches(result, ">1<") == 2);
-		assertTrue(StringUtils.countMatches(result, ">2<") == 2);
+        assertTrue(result.contains(">1<"));
+        assertTrue(result.contains(">2<"));
 
-	}
+        assertTrue(StringUtils.countMatches(result, ">1<") == 2);
+        assertTrue(StringUtils.countMatches(result, ">2<") == 2);
 
-	@Test
-	public void testProcessScriptedTemplate_logicScriptlets() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-9.xml");
+    }
 
-		HashMap<String, Object> params1 = new HashMap<String, Object>();
-		params1.put("value", 1);
+    @Test
+    public void testProcessScriptedTemplate_logicScriptlets() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-9.xml");
 
-		String result = DocxTemplater.processScriptedTemplate(template, params1);
+        HashMap<String, Object> params1 = new HashMap<String, Object>();
+        params1.put("value", 1);
 
-		assertTrue(result != null);
-		assertTrue(!result.contains("else"));
-		assertTrue(!result.contains("if"));
+        String result = DocxTemplater.processScriptedTemplate(template, params1);
 
-		assertTrue(result.contains("mom and dad"));
-		assertTrue(result.contains("like kitties"));
+        assertTrue(result != null);
+        assertTrue(!result.contains("else"));
+        assertTrue(!result.contains("if"));
 
-		params1 = new HashMap<String, Object>();
-		params1.put("value", 0);
+        assertTrue(result.contains("mom and dad"));
+        assertTrue(result.contains("like kitties"));
 
-		result = DocxTemplater.processScriptedTemplate(template, params1);
+        params1 = new HashMap<String, Object>();
+        params1.put("value", 0);
 
-		assertTrue(result != null);
-		assertTrue(!result.contains("else"));
-		assertTrue(!result.contains("if"));
+        result = DocxTemplater.processScriptedTemplate(template, params1);
 
-		assertTrue(result.contains("mom and dad"));
-		assertTrue(result.contains("like dogs"));
-	}
+        assertTrue(result != null);
+        assertTrue(!result.contains("else"));
+        assertTrue(!result.contains("if"));
 
-	@Test
-	public void testProcessScriptedTemplate_spacePreserve() throws Exception {
-		String template = TestUtils.readResource("/docx/DocxTemplaterTest-10.xml");
+        assertTrue(result.contains("mom and dad"));
+        assertTrue(result.contains("like dogs"));
+    }
 
-		String result = DocxTemplater.processScriptedTemplate(template, params);
+    @Test
+    public void testProcessScriptedTemplate_spacePreserve() throws Exception {
+        String template = TestUtils.readResource("/docx/DocxTemplaterTest-10.xml");
 
-		assertTrue(result != null);
-		assertTrue(!result.contains("print"));
+        String result = DocxTemplater.processScriptedTemplate(template, params);
 
-		assertTrue(result.contains("like dogs"));
-	}
+        assertTrue(result != null);
+        assertTrue(!result.contains("print"));
 
-	@Test
-	public void testProcess() throws Exception {
-		File inFile = File.createTempFile("DocxTemplaterTest-1", ".docx");
-		inFile.deleteOnExit();
-		File resFile = File.createTempFile("DocxTemplaterTest-1-result", ".docx");
-		resFile.deleteOnExit();
+        assertTrue(result.contains("like dogs"));
+    }
 
-		TestUtils.copyResourceToFile("/docx/DocxTemplaterTest-1.docx", inFile);
+    @Test
+    public void testProcess() throws Exception {
+        File inFile = File.createTempFile("DocxTemplaterTest-1", ".docx");
+        inFile.deleteOnExit();
+        File resFile = new File("target/test-files/DocxTemplaterTest-1-result.docx");
+        resFile.delete();
 
-		DocxTemplater docxTemplater = new DocxTemplater(inFile);
+        TestUtils.copyResourceToFile("/docx/DocxTemplaterTest-1.docx", inFile);
 
-		docxTemplater.process(resFile, params);
+        DocxTemplater docxTemplater = new DocxTemplater(inFile);
 
-		assertTrue(resFile.exists());
-		assertTrue(resFile.length() > 0);
-	}
+        docxTemplater.process(resFile, params);
+
+        assertTrue(resFile.exists());
+        assertTrue(resFile.length() > 0);
+    }
 
 }
