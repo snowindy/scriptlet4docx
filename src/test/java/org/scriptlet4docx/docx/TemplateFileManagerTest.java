205281128e85e0caab8605620c3ccbd977f132ad
eg <blackorangebox@gmail.com>
2013-05-15 11:01:46 +0800

    fixed isTemplateFileFromStreamExists

--- src/test/java/org/scriptlet4docx/docx/TemplateFileManagerTest.java
+++ src/test/java/org/scriptlet4docx/docx/TemplateFileManagerTest.java
@@ -13,7 +13,9 @@ public class TemplateFileManagerTest extends Assert {
         String templateKey = "k1";
         File tempDir = mgr.getTemplatesDir();
         File docxFile = new File("src/test/resources/docx/DocxTemplaterTest-1.docx");
+        assertFalse(mgr.isPrepared(templateKey));
         mgr.prepare(docxFile, templateKey);
+        assertTrue(mgr.isPrepared(templateKey));
         assertFalse(mgr.getTemplateContent(templateKey).isEmpty());
         assertEquals(mgr.getTemplateUnzipFolder(templateKey), new File(tempDir, templateKey + "/"
                 + TemplateFileManager.DOC_UNIZIP_FOLDER_NAME));
@@ -22,9 +24,9 @@ public class TemplateFileManagerTest extends Assert {
         assertTrue(mgr.isPreProcessedTemplateExists(templateKey));
         assertEquals("1", mgr.getTemplateContent(templateKey));
 
-        assertFalse(mgr.isPreProcessedTemplateExists(templateKey));
+        assertFalse(mgr.isTemplateFileFromStreamExists(templateKey));
         mgr.saveTemplateFileFromStream(templateKey, new FileInputStream(docxFile));
-        assertTrue(mgr.isPreProcessedTemplateExists(templateKey));
+        assertTrue(mgr.isTemplateFileFromStreamExists(templateKey));
 
         mgr.cleanup();
         assertTrue(tempDir.exists());
